b65a3101bfab5bc3557b1f23e46b3676355f3acd
Tom Wilson <tom@jackhq.com>
2017-10-19 15:48:44 -0400

    added item list to index

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const htmlparser = require('htmlparser2')
-const ItemList = require('./item-list')
 const R = require('ramda')
 const {
   omit,
@@ -118,4 +117,38 @@ function renderReturn(attribs, elementContent) {
   return function(tag) {
     return `return ${trim(elementContent)}`
   }
+}
+
+function ItemList (parent) {
+  this.parent = parent
+  this.content = ''
+  this.spacer = ' '
+  this.indent = parent ? parent.indent : ''
+  this.isFirstItem = true
+}
+
+ItemList.prototype.addSpace = function(space) {
+  this.spacer += space
+
+  if (space.indexOf('\n') !== -1) {
+    // reset indent when there are new lines
+    this.indent = /[^\n]*$/.exec(space)[0]
+  } else {
+    // otherwise keep appending to current indent
+    this.indent += space
+  }
+}
+
+ItemList.prototype.add = function(data, ignoreComma) {
+  // if (!ignoreComma) {
+  //   if (!this.isFirstItem) {
+  //     this.content += this.spacer.length ? ',' : ', '
+  //   }
+  //
+  //   this.isFirstItem = false
+  // }
+  //
+  // this.content += this.spacer
+  // this.spacer = ''
+  this.content += data
 }
